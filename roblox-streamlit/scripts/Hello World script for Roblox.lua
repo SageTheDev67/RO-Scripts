fdfb852a0afcaedc544d7b3517af07ec1e50effe
SageDev <lianafinch38@gmail.com>
2025-09-29 12:25:06 +0100

    Add files via upload

---
+++ roblox-streamlit/scripts/Hello World script for Roblox.lua
@@ -0,0 +1,13 @@
+-- Hello World script for Roblox
+-- Author: Eviie
+-- Category: Tutorial
+
+print("Hello, Roblox world!")
+
+-- Example of a simple function
+local function greetPlayer(playerName)
+    print("Welcome to the game, " .. playerName .. "!")
+end
+
+-- Call the function
+greetPlayer("Eviie")
